5c42a59845f6bf7f4700a0722b701c308bbcf3a8
Lazaro Raul <riglesiasvera@gmail.com>
2020-12-07 01:10:48 -0500

    Adding real data

--- js/function.js
+++ js/function.js
@@ -29,32 +29,59 @@ function new_bar(parity, label, percent){
   return `<div class="bar" data-percent="${percent}%"><span class="${cls[parity]}">${label}</span></div>\n`
 };
 
-function load(json){
+function load(json, id){
   //TODO: get the json and remove the function argument
-  var val = document.getElementById('strategy').value;
-  if(val in json){
-    document.getElementById('main-label').innerHTML = val;
-    var data = json[val];
+  json = json_data(json, id);
+  var label = document.getElementById(`id${id}`).value;
+  document.getElementById('main-label').innerHTML = label;
+  var data = json[label];
 
-    var p = 0;
-    var lines = "";
-    for(var key in data){
-      if(key !== val){
-        lines += new_bar(p, key, data[key]);
-        p = 1 - p;
-      }
+  var p = 0;
+  var lines = "";
+  for(var key in data){
+    if(key !== label){
+      lines += new_bar(p, key, data[key]);
+      p = 1 - p;
     }
-    document.getElementById('holder').innerHTML = lines;
-    refresh_charts();
   }
+  document.getElementById('holder').innerHTML = lines;
+  refresh_charts();
 };
 
-function populate_select(json){
+function json_data(json, k){
+  for(var i = 1; i < k; i++){
+    console.log(i);
+    json = json[document.getElementById(`id${i}`).value];
+  }
+  return json
+}
+
+function upd_data(json, i, j){
+  console.log(i, j);
+  for(var id = i; id <= j; id++){
+    populate_select(json_data(json, id), id);
+  }
+  load(json, j);
+}
+
+function populate_select(json, i){
   //TODO: get the json and remove the function argument
-  var sel = document.getElementById('strategy');
+  console.log(i);
+  var sel = document.getElementById(`id${i}`);
   lines = "";
   for(var key in json){
     lines += `<option value="${key}">${key}</option>\n`;
   }
   sel.innerHTML = lines;
+};
+
+function populate_selectors(n){
+  //TODO: get the json and remove the function argument
+  var div = document.getElementById('selectors');
+  lines = "";
+  for(var i = 1; i < n; i++){
+    lines += `<select name='id${i}' id='id${i}' onchange="upd_data(SITE_JSON, ${i + 1}, ${n});"></select>\n`;
+  }
+  lines += `<select name='id${n}' id='id${n}' onchange="load(SITE_JSON, ${n});"></select>\n`;
+  div.innerHTML = lines;
 };
