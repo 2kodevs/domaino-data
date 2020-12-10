5ca4c6e1c6083f22be8c8da75fc0b103b52ee61b
Carmen I Cabrera <kr1000a@gmail.com>
2020-12-10 16:19:29 -0500

    change method to compute mean of victories for strategy in 9x9

--- utils/detect_best.py
+++ utils/detect_best.py
@@ -1,6 +1,6 @@
 import json
 
-data = json.load(open('data/data.json', 'r'))
+data = json.load(open('../data/data.json', 'r'))
 
 strategies = {
     'Normal': {},
@@ -9,16 +9,16 @@ strategies = {
 strategies55 = {}
 counts = {}
 
-for game in data.values():
-    for rule in game.values():
-        for behavior in rule.values():
-            for prefix, d_prefix in behavior.items():
-                for strategy, d_strategy in d_prefix.items():
-                    k = f'{prefix}-{strategy}'
-                    for v in d_strategy.values():
-                        strategies[prefix][k] = strategies[prefix].get(k, 0) + v
-                        strategies55[k] = strategies55.get(k, True) and v >= 55
-                    counts[k] = counts.get(k, 0) + len(d_strategy)
+
+for rule in data['9x9'].values():
+    for behavior in rule.values():
+        for prefix, d_prefix in behavior.items():
+            for strategy, d_strategy in d_prefix.items():
+                k = f'{prefix}-{strategy}'
+                for v in d_strategy.values():
+                    strategies[prefix][k] = strategies[prefix].get(k, 0) + v
+                    strategies55[k] = strategies55.get(k, True) and v >= 55
+                counts[k] = counts.get(k, 0) + len(d_strategy)
 
 best_by_mean = {
     'Normal':     [],
@@ -40,7 +40,7 @@ for v in best_by_mean.values():
 print('Best by mean:')
 for k, v in best_by_mean.items():
     print(k)
-    for s in v[:5]:
+    for s in v:
         print(s)
 
 print('Strategies with absolute values greater than 55:')
